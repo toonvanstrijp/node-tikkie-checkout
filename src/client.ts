6d8adf9cea142d87f10a5b6c680a9e157de92aa1
Toon van Strijp <toonvanstrijp@gmail.com>
2019-12-27 16:23:11 +0100

    disable authorization header when sandbox mode

--- src/client.ts
+++ src/client.ts
@@ -1,33 +1,41 @@
-
-import {TikkieCheckoutConfig} from './config';
+import { TikkieCheckoutConfig } from './config';
 import { CreatedOrder } from './models/createdOrder';
 import { Order } from './models/order';
 import { CreateOrder } from './models/createOrder';
 
 export class TikkieCheckoutClient {
-    config: TikkieCheckoutConfig;
+  config: TikkieCheckoutConfig;
 
-    /**
-     * Initialize TikkieCheckoutClient.
-     *
-     * @param {TikkieCheckoutConfig} config Configuration object.
-     */
-    constructor(config: TikkieCheckoutConfig) {
-        this.config = config;
+  /**
+   * Initialize TikkieCheckoutClient.
+   *
+   * @param {TikkieCheckoutConfig} config Configuration object.
+   */
+  constructor(config: TikkieCheckoutConfig) {
+    this.config = config;
+  }
+
+  /**
+   * Manually authenticate with the Tikkie API.
+   * TikkieCheckoutClient will automatically connect before making the first request.
+   */
+  async authenticate(): Promise<void> {
+    try {
+      await this.config.getAccessToken();
+    } catch (err) {
+      throw err;
     }
+  }
 
-    /**
-     * Manually authenticate with the Tikkie API.
-     * TikkieCheckoutClient will automatically connect before making the first request.
-     */
-    async authenticate(): Promise<void> {
-        try {
-            await this.config.getAccessToken();
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    createOrder(data: CreateOrder) { return this.config.postRequest<CreatedOrder>('/v1/tikkie/fastcheckout/orders', data); }
-    getOrder(orderToken: string) { return this.config.getRequest<Order>(`/v1/tikkie/fastcheckout/orders/${orderToken}`); }
+  createOrder(data: CreateOrder) {
+    return this.config.postRequest<CreatedOrder>(
+      '/v1/tikkie/fastcheckout/orders',
+      data,
+    );
+  }
+  getOrder(orderToken: string) {
+    return this.config.getRequest<Order>(
+      `/v1/tikkie/fastcheckout/orders/${orderToken}`,
+    );
+  }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 import * as fs from 'fs';
-import {URLSearchParams, format} from 'url';
+import { URLSearchParams, format } from 'url';
+import querystring from 'querystring';
+
 import jwt from 'jsonwebtoken';
 
-import {AccessToken} from './accessToken';
-import {TikkieErrorCollection} from './error';
+import { AccessToken } from './accessToken';
+import { TikkieErrorCollection } from './error';
 
 const PRODUCTION_API_URL = 'https://api.abnamro.com';
 const PRODUCTION_TOKEN_AUDIENCE = 'https://auth.abnamro.com/oauth/token';
@@ -11,120 +13,157 @@ const SANDBOX_API_URL = 'https://api-sandbox.abnamro.com';
 const SANDBOX_TOKEN_AUDIENCE = 'https://auth-sandbox.abnamro.com/oauth/token';
 
 export class TikkieCheckoutConfig {
-    accessToken: AccessToken;
-    apiKey: string;
-    merchantToken: string;
-    useSandbox: boolean;
-    apiUrl: string;
-    tokenAudience: string;
-    privateKey: string;
-    algorithm: string;
+  accessToken: AccessToken;
+  apiKey: string;
+  merchantToken: string;
+  useSandbox: boolean;
+  apiUrl: string;
+  tokenAudience: string;
+  privateKey: string;
+  algorithm: string;
 
-    constructor(apiKey: string, merchantToken: string, useSandbox: boolean = false) {
-        this.apiKey = apiKey;
-        this.merchantToken = merchantToken;
-        this.useSandbox = useSandbox;
-        this.apiUrl = useSandbox ? SANDBOX_API_URL : PRODUCTION_API_URL;
-        this.tokenAudience = useSandbox ? SANDBOX_TOKEN_AUDIENCE : PRODUCTION_TOKEN_AUDIENCE;
-    }
+  constructor(
+    apiKey: string,
+    merchantToken: string,
+    useSandbox: boolean = false,
+  ) {
+    this.apiKey = apiKey;
+    this.merchantToken = merchantToken;
+    this.useSandbox = useSandbox;
+    this.apiUrl = useSandbox ? SANDBOX_API_URL : PRODUCTION_API_URL;
+    this.tokenAudience = useSandbox
+      ? SANDBOX_TOKEN_AUDIENCE
+      : PRODUCTION_TOKEN_AUDIENCE;
+  }
 
-    loadPrivateKey(path: string, algorithm: string = 'RS256') {
-        this.loadPrivateKeyFromString(fs.readFileSync(path, 'utf8'), algorithm);
-    }
+  loadPrivateKey(path: string, algorithm: string = 'RS256') {
+    this.loadPrivateKeyFromString(fs.readFileSync(path, 'utf8'), algorithm);
+  }
 
-    loadPrivateKeyFromString(privateKey: string, algorithm: string = 'RS256') {
-        this.privateKey = privateKey;
-        this.algorithm = algorithm;
-    }
+  loadPrivateKeyFromString(privateKey: string, algorithm: string = 'RS256') {
+    this.privateKey = privateKey;
+    this.algorithm = algorithm;
+  }
 
-    createHeaders(): Headers {
-        const headers: Headers = new Headers();
-        headers.append('User-Agent', 'node-tikkie-checkout/1.0');
-        headers.append('API-Key', this.apiKey);
-        headers.append('X-Merchant-Token', this.merchantToken);
-        return headers;
-    }
+  createHeaders(): Headers {
+    const headers: Headers = new Headers();
+    headers.append('User-Agent', 'node-tikkie-checkout/1.0');
+    headers.append('API-Key', this.apiKey);
+    headers.append('X-Merchant-Token', this.merchantToken);
+    return headers;
+  }
 
-    getJsonWebToken(): string {
-        return jwt.sign({}, this.privateKey, {
-            algorithm: this.algorithm,
-            expiresIn: '3m',
-            notBefore: '-1m',
-            issuer: 'node-tikkie-checkout',
-            subject: this.apiKey,
-            audience: this.tokenAudience,
-        });
-    }
+  getJsonWebToken(): string {
+    return jwt.sign({}, this.privateKey, {
+      algorithm: this.algorithm,
+      expiresIn: '3m',
+      notBefore: '-1m',
+      issuer: 'node-tikkie-checkout',
+      subject: this.apiKey,
+      audience: this.tokenAudience,
+    });
+  }
 
-    async getAccessToken(): Promise<string> {
-        if (!this.accessToken || this.accessToken.hasExpired()) {
-            try {
-                const body = new URLSearchParams();
-                body.append('client_assertion_type', 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer');
-                body.append('client_assertion', this.getJsonWebToken());
-                body.append('grant_type', 'client_credentials');
-                body.append('scope', 'tikkie');
+  async getAccessToken(): Promise<string> {
+    if (!this.accessToken || this.accessToken.hasExpired()) {
+      try {
+        const body = new URLSearchParams();
+        body.append(
+          'client_assertion_type',
+          'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+        );
+        body.append('client_assertion', this.getJsonWebToken());
+        body.append('grant_type', 'client_credentials');
+        body.append('scope', 'tikkie');
 
-                const headers: Headers = this.createHeaders();
-                headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        const headers: Headers = this.createHeaders();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-                const response: Response = await fetch(`${this.apiUrl}/v1/oauth/token`, {
-                    method: 'POST',
-                    headers,
-                    body,
-                });
+        const response: Response = await fetch(
+          `${this.apiUrl}/v1/oauth/token`,
+          {
+            method: 'POST',
+            headers,
+            body,
+          },
+        );
 
-                const result = await response.json();
+        const result = await response.json();
 
-                if (response.status >= 200 && response.status <= 399) {
-                    this.accessToken = new AccessToken(result);
-                } else {
-                    throw new TikkieErrorCollection(result.errors);
-                }
-            } catch (err) {
-                throw err;
-            }
+        if (response.status >= 200 && response.status <= 399) {
+          this.accessToken = new AccessToken(result);
+        } else {
+          throw new TikkieErrorCollection(result.errors);
         }
-        return this.accessToken.token;
+      } catch (err) {
+        throw err;
+      }
     }
+    return this.accessToken.token;
+  }
 
-    async request<T>(method: 'GET' | 'POST', endpoint: string, data: object | null = null): Promise<T> {
+  async request<T>(
+    method: 'GET' | 'POST',
+    endpoint: string,
+    options: {
+      query: { [key: string]: string } | null;
+      data: object | null;
+    } = { query: null, data: null },
+  ): Promise<T> {
+    try {
+      const headers: Headers = this.createHeaders();
+
+      // Authorization only needed for production
+      if (!this.useSandbox) {
+        let token: string;
         try {
-            let token: string;
-            try {
-                token = await this.getAccessToken();
-            } catch (err) {
-                throw err;
-            }
-
-            const headers: Headers = this.createHeaders();
-            headers.append('Authorization', `Bearer ${token}`);
-            if (method === 'POST' && data) {
-                headers.append('Content-Type', 'application/json');
-            }
-
-            let queryString = '';
-            if (method === 'GET' && data) {
-                queryString = format({query: data});
-            }
-
-            const response: Response = await fetch(`${this.apiUrl}${endpoint}${queryString}`, {
-                method,
-                headers,
-                body: (method === 'POST' && data) ? JSON.stringify(data) : undefined,
-            });
-            const result = await response.json();
-
-            if (response.status >= 200 && response.status <= 399) {
-                return result;
-            } else {
-                throw new TikkieErrorCollection(result.errors);
-            }
+          token = await this.getAccessToken();
         } catch (err) {
-            throw err;
+          throw err;
         }
-    }
 
-    getRequest<T>(endpoint: string, query: object = {}): Promise<T> { return this.request('GET', endpoint, query); }
-    postRequest<T>(endpoint: string, data: object = {}): Promise<T> { return this.request<T>('POST', endpoint, data); }
+        headers.append('Authorization', `Bearer ${token}`);
+      }
+
+      if (method === 'POST' && options.data) {
+        headers.append('Content-Type', 'application/json');
+      }
+
+      let queryString = '';
+      if (method === 'GET' && options.query) {
+        queryString = querystring.stringify(options.query);
+      }
+
+      const response: Response = await fetch(
+        `${this.apiUrl}${endpoint}${queryString}`,
+        {
+          method,
+          headers,
+          body:
+            method === 'POST' && options.data
+              ? JSON.stringify(options.data)
+              : undefined,
+        },
+      );
+      const result = await response.json();
+
+      if (response.status >= 200 && response.status <= 399) {
+        return result;
+      } else {
+        throw new TikkieErrorCollection(result.errors);
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  getRequest<T>(
+    endpoint: string,
+    query: { [key: string]: string } = {},
+  ): Promise<T> {
+    return this.request('GET', endpoint, { query, data: null });
+  }
+  postRequest<T>(endpoint: string, data: object = {}): Promise<T> {
+    return this.request<T>('POST', endpoint, { data, query: null });
+  }
 }
